aa9419baf77be002d883f1e3a1cb2562c1d77ae4
Corentin Eder <76168237+etharion29@users.noreply.github.com>
2023-03-05 12:05:56 +0100

    Vue test chart init

---
+++ src/components/ChartTest.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <canvas id="chart-test" height="90"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+import chartTestData from '../components/chart-test-data'
+
+export default {
+  name: 'ChartTest',
+  data() {
+    return {
+      chartTestData: chartTestData
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('chart-test');
+    new Chart(ctx, this.chartTestData);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
